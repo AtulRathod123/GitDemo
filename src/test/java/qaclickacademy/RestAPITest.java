0f6c1b27f3cf96de05be0731c9e4e6beb9eaefb1
Atul <atul.trinyx@gmail.com>
2020-02-24 14:51:48 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void postjirs()
+	{
+		System.out.println("postjirs");
+
+	}
+	@Test
+	public void deletwitter()
+	{
+		System.out.println("deletwitter");
+
+	}
+
+}
